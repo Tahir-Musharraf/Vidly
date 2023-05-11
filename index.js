622253419e152fc544d786f3fa0390b8c67aea65
Tahir Musharraf <tmusharraf6@gmail.com>
2023-05-11 10:39:50 +0500

    Update Getting all Movies

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const movies = [
+    { id: 1, name: 'Jhon Wick', genres: 'Action'},
+    { id: 2, name: 'IronMan', genres: 'Action & Thriller'},
+    { id: 3, name: 'Avengers', genres: 'Action & Adventure'},
+    { id: 4, name: 'Ant Man', genres: 'Sci-fi'},
+    { id: 5, name: 'Intestaller', genres: 'Adventure & Sci-fi'}
+]
+
+// GET all movies
+app.get("/api/movies/", (req, res) => {
+    res.send(movies)
+})
