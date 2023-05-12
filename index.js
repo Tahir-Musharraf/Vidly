a4dfe94d013f3b6cccd41e4c09c8d691f5034fbd
Tahir Musharraf <tmusharraf6@gmail.com>
2023-05-12 10:31:37 +0500

    Update Showing all the movie with matching genre

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const app = express();
 
+// app.use(express.json());
+
 const movies = [
     { id: 1, name: 'Jhon Wick', genres: 'Action'},
-    { id: 2, name: 'IronMan', genres: 'Action & Thriller'},
-    { id: 3, name: 'Avengers', genres: 'Action & Adventure'},
+    { id: 2, name: 'IronMan', genres: 'Action, Thriller'},
+    { id: 3, name: 'Avengers', genres: 'Action, Adventure'},
     { id: 4, name: 'Ant Man', genres: 'Sci-fi'},
-    { id: 5, name: 'Intestaller', genres: 'Adventure & Sci-fi'}
+    { id: 5, name: 'Intestaller', genres: 'Adventure, Sci-fi'},
+    { id: 5, name: 'God Zila', genres: 'Action'}
 ]
+//Home Page 
+app.get("/", (req, res) => {
+    res.send("Welcome!")
+})
 
 // GET all movies
 app.get("/api/movies/", (req, res) => {
     res.send(movies)
 })
+// GET single movie
+app.get("/api/movies/:genre", (req, res) => {
+    const movie = movies.filter(movie => movie.genres.includes(req.params.genre))
+    console.log(movie)
+    if (!movie) return res.status(404).send("The required genre not found!")
+
+    res.send(movie)
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Lisiting on " + port + "..."))
