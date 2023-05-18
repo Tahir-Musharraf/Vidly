6e3adb74c8262871ae7e62c71fff90fadea55de4
Tahir Musharraf <tmusharraf6@gmail.com>
2023-05-18 11:39:42 +0500

    Updating welcome message

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 router.get("/", (req, res) => {
-    res.send("Welcome!")
+    res.send("Welcome to Movie APIs!")
 })
 
 module.exports = router
